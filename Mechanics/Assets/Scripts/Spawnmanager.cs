ba842066d3bfe21c9d97c327fa3dc384398029e1
Tristan Tricoche <tricochet@roverkids.org>
2025-01-07 13:44:11 -0500

    randomly generates spawn position

--- Mechanics/Assets/Scripts/Spawnmanager.cs
+++ Mechanics/Assets/Scripts/Spawnmanager.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class Spawnmanager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnposZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnposZ);
+
         Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
     }
 
